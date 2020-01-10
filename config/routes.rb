4a8cbe625a0395d5e1f49d997cc35067c59c15b2
Lukas Corbitt <movalley5@gmail.com>
2020-01-09 21:53:08 -0700

    Added confirmations_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   as :user do
-    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :udate_user_confirmation 
+    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation 
   end
 
   devise_for :users, controllers: {
     registrations: 'registrations',
-    confirmation: 'confirmations'
+    confirmations: 'confirmations'
   }
   root 'home#index'
 end
